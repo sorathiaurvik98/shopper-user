7b50f32abd0e3be82b2c989c22a42dccf587359d
Urvik Sorathia <urvik.sorathia@brevitaz.com>
2022-03-08 00:44:41 +0530

    Added project skeleton

---
+++ src/main/java/com/shopper/shopperuser/ShopperUserApplication.java
@@ -0,0 +1,16 @@
+package com.shopper.shopperuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableSwagger2
+public class ShopperUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopperUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopper/shopperuser/ShopperUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopper.shopperuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
